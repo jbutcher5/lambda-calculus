27c4329e6eae9bba8f70d54a849b2fd4d8239507
James Butcher <jamesbutcher167@gmail.com>
2023-02-04 16:10:45 +0000

    Refactor lexer

--- src/main.c
+++ src/main.c
@@ -4,10 +4,20 @@
 
 #define TOKEN_BUFFER_INIT_SIZE 256
 
+// Character significance in grammar
+
+static const char SPECIAL[] = ".\\=\n";
+static const char IGNORE[] = " ";
+
+// Lexer structure
+
 typedef enum {
-  Dot,
-  BackSlash,
-  Ident
+  Dot = 0,
+  BackSlash = 1,
+  Equals = 2,
+  Newline = 3,
+  End = 4,
+  Ident = 5
 } TokenType;
 
 typedef struct {
@@ -15,56 +25,44 @@ typedef struct {
   int start, end;
 } LexerToken;
 
-LexerToken *parser(const char *input) {
+LexerToken *lexer(const char *input) {
   LexerToken current;
-  LexerToken *buffer = (LexerToken*)calloc(sizeof(LexerToken), 256);
+  LexerToken *buffer = (LexerToken*)calloc(sizeof(LexerToken), TOKEN_BUFFER_INIT_SIZE);
   int current_empty = 1;
   int buffer_len = 0;
-  
-  for (int i = 0; i < strlen(input); i++) {
+
+  for (int i = 0; i < strlen(input) + 1; i++) { 
     const char c = input[i];
 
-    switch (c) {
-      case '.':
-        if (!current_empty) {
-          buffer[buffer_len] = current;
-          current_empty = 1;
-          buffer_len++;
-        }
+    // Check if c is in ignore list
+    
+    int j = 0;
+    for (; j < strlen(IGNORE); j++)
+      if (IGNORE[j] == c) break;
+    if (j < strlen(IGNORE)) continue;
 
-        buffer[buffer_len] = (LexerToken){.type = Dot, .start = i, .end = i};
-        buffer_len++;
-        break;
-      
-      case '\\':
-        if (!current_empty) {
-          buffer[buffer_len] = current;
-          current_empty = 1;
-          buffer_len++;
-        }
+    // Check is c is in special list
 
-        buffer[buffer_len] = (LexerToken){.type = BackSlash, .start = i, .end = i};
-        buffer_len++;
-        break;
+    int token_type = 0;
+    for (; token_type < strlen(SPECIAL) + 1; token_type++)
+      if (SPECIAL[token_type] == c) break;
 
-      case ' ':
-        if (!current_empty) {
-          buffer[buffer_len] = current;
-          current_empty = 1;
-          buffer_len++;
-        }
-        break;
-      
-      default:
-        if (current_empty) {
-          current = (LexerToken){.type = Ident, .start = i, .end = i};
-          current_empty = 0;  
-        } else {
-          current.end++;
-        }
-        break;
+    if (token_type != Ident && !current_empty) {
+      buffer[buffer_len] = current;
+      current_empty = 1;
+      buffer_len++;
+
+      buffer[buffer_len] = (LexerToken){.type = token_type, .start = i, .end = i};
+      buffer_len++;
+    } else if (token_type < Ident && current_empty) {
+      buffer[buffer_len] = (LexerToken){.type = token_type, .start = i, .end = i};
+      buffer_len++;
+    } else if (token_type == Ident && current_empty) {
+      current = (LexerToken){.type = Ident, .start = i, .end = i};
+      current_empty = 0;
+    } else {
+      current.end++;
     }
-      
   }
   
   if (!current_empty) {
@@ -78,8 +76,7 @@ LexerToken *parser(const char *input) {
 
 int main() {
 
-  LexerToken *buffer = parser("\\x.y x");
-
+  LexerToken *buffer = lexer("\\x.y x");
 
   return 0;
   
